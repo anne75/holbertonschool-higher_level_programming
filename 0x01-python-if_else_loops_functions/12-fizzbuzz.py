272e72dffcf1cd4fc61ff7f6de67d7253401a3ed
Anne <anne.cognet@holbertonschool.com>
2017-01-03 23:55:42 +0000

    second commit

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if (n % 3 == 0):
+            print("Fizz", end="")
+        if (n % 5 == 0):
+            print("Buzz", end="")
+        elif (n % 3 != 0):
+            print("{:d}".format(n), end="")
+        print(" ", end="")
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    n = number % 10
+    n = abs(number) % 10
     print("{:d}".format(n), end="")
     return (n)
