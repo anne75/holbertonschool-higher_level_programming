4400ac4e5dd24b1c2ca5a8e48bdeabad45e38e22
Anne <anne.cognet@holbertonschool.com>
2017-01-04 00:46:22 +0000

    new version

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,26 @@
 #!/usr/bin/python3
+# def fizzbuzz():
+#     for n in range(1, 101):
+#         if (n % 3 == 0):
+#             print("Fizz", end="")
+#         if (n % 5 == 0):
+#             print("Buzz", end="")
+#         elif (n % 3 != 0):
+#             print("{:d}".format(n), end="")
+#         print(" ", end="")
+
+
+def helper(n):
+    s = ""
+    if (n % 3 == 0):
+        s += "Fizz"
+    if (n % 5 == 0):
+        s += "Buzz"
+    elif (n % 3 != 0):
+        s += str(n)
+    return (s)
+
+
 def fizzbuzz():
-    for n in range(1, 101):
-        if (n % 3 == 0):
-            print("Fizz", end="")
-        if (n % 5 == 0):
-            print("Buzz", end="")
-        elif (n % 3 != 0):
-            print("{:d}".format(n), end="")
-        print(" ", end="")
+    print(" ".join([helper(n) for n in range(1, 101)]), end="")
+    print("{} ".format(""), end="")
