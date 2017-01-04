5cb91b6d5714b677dc66cd63a2bfd7ea0c66f117
Anne <anne.cognet@holbertonschool.com>
2017-01-04 00:32:36 +0000

    old hack

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-# for i in range(1):
-#     print("{}".format("zYxWvUtSrQpOnMlKjIhGfEdCbA"), end="")
-
 for i in range(1):
-    print("zYxWvUtSrQpOnMlKjIhGfEdCbA", end="")
-    print("{}".format(""), end="")
+    print("{}".format("zYxWvUtSrQpOnMlKjIhGfEdCbA"), end="")
