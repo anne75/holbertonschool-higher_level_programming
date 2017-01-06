a76ef24ecd061c872802caf7138eff9b0077143f
Anne <anne.cognet@holbertonschool.com>
2017-01-06 00:27:07 +0000

    dict are not ordered

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     a = 10
     b = 5
     d = {"+": add, "-": sub, "*": mul, "/": div}
-    for key in d:
-        print("{:d} {:s} {:d} = {:d}".format(a, key, b, d[key](a, b)))
+    for op in "+-*/":
+        print("{:d} {:s} {:d} = {:d}".format(a, op, b, d[op](a, b)))
