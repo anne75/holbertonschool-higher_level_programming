98919a2b0d2c271cb2d79ba1c13284e2d9596f1e
Anne <anne.cognet@holbertonschool.com>
2017-01-06 00:41:24 +0000

    empty module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    print("\n".join([x for x in dir(hidden_4) if not x[:2] == "__"]))
+    correct = [x for x in dir(hidden_4) if not x[:2] == "__"]
+    if len(correct):
+        print("\n".join(correct))
