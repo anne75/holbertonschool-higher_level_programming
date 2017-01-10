f8de2f19ce96d6c5dbb83d4bf453f6f6a1de8573
Anne <anne.cognet@holbertonschool.com>
2017-01-10 22:21:32 +0000

    none list

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -5,5 +5,5 @@ def print_list_integer(mylist=[]):
     """
     print the elements of a list of int
     """
-    if (mylist):
+    if (mylist and mylist != None):
         print("\n".join("{:d}".format(x) for x in mylist))
