581c5c7a06354dbb1767403eee1086e8ceca60c3
Anne <anne.cognet@holbertonschool.com>
2017-01-10 22:27:58 +0000

    change cpmments

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -6,8 +6,8 @@ def new_in_list(li, idx, element):
     create a copy of a list, modifying the element
     at idx if idx in the right range
 
-    DO NOT use try/except
-    DO NOT consider neagive indexes
+    DO NOT catch
+    DO NOT consider negative indexes
     """
     new = li[:]
     size = len(li)
