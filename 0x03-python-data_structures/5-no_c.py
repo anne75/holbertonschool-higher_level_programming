df3bb689fee4d446f0f3458354994d64eebc617b
Anne <anne.cognet@holbertonschool.com>
2017-01-10 22:30:18 +0000

    change comments

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -4,7 +4,7 @@
 def no_c(mystr):
     """
     removes all letter c from a string
-    DO NOT use str.replace()
+    DO NOT use s t r . r e p l a c e
 
     """
     my_list = [letter for letter in mystr if (letter.lower() != "c")]
