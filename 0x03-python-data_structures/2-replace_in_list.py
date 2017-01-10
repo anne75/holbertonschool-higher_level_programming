f76f72330641659c10237b28a22e1cdf45b02985
Anne <anne.cognet@holbertonschool.com>
2017-01-10 23:05:34 +0000

    no comment

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,14 +2,8 @@
 
 
 def replace_in_list(mylist, idx, element):
-    """
-    replaces an element of a list at index
-    if idx is out of range do not modify the list
-
-    DO NOT catch
-    DO NOT consider negative indexes
-    """
-    size = len(mylist)
-    if (0 <= idx < size):
-        mylist[idx] = element
+    if mylist:
+        size = len(mylist)
+        if (0 <= idx < size):
+            mylist[idx] = element
     return (mylist)
