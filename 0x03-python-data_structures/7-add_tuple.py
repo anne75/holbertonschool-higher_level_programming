7fa75fc749f295850748618fd168e21fdf721ca6
Anne <anne.cognet@holbertonschool.com>
2017-01-10 22:49:21 +0000

    none of them is good

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,9 +8,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     pre element
     """
     if not tuple_a:
-        return (tuple_b)
+        return add_tuple(tuple_b, (0, 0))
     if not tuple_b:
-        return (tuple_a)
+        return add_tuple((tuple_a), (0, 0))
 
     x = tuple_a[0] + tuple_b[0]
     y = 0 if len(tuple_a) == 1 else tuple_a[1]
