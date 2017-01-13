4a9094703248390a39c1497d5cc84e564281a139
Anne <anne.cognet@holbertonschool.com>
2017-01-13 22:41:44 +0000

    add error

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,7 +14,7 @@ def safe_print_list(my_list=[], x=0):
     while count < x:
         try:
             print("{}".format(my_list[count]), end="")
-        except (IndexError, TypeError):
+        except (IndexError, TypeError, ValueError):
             break
         count += 1
     if (count):
