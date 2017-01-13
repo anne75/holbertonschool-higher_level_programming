10ad05286a1382b4bf40d4a42c5817b51aa235cc
Anne <anne.cognet@holbertonschool.com>
2017-01-13 22:39:40 +0000

    first commit

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """
+    raise a Name exception with a message
+
+    message: a string
+    """
+    raise NameError(message)
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+Project around Exceptions
