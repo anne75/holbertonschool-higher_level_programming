18ceaefd5fb24707e567d2294aab13cb29135a2a
Anne <anne.cognet@holbertonschool.com>
2017-01-13 22:46:16 +0000

    no check on new line

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -17,6 +17,4 @@ def safe_print_list_integers(my_list=[], x=0):
             count += 1
         except (TypeError, ValueError):
             continue
-    if (count):
-        print("")
     return (count)
