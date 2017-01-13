1aab803cba488382c6f3a4e1226f3465e9ccf2c1
Anne <anne.cognet@holbertonschool.com>
2017-01-13 22:45:03 +0000

    no check on new line

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -14,9 +14,8 @@ def safe_print_list(my_list=[], x=0):
     while count < x:
         try:
             print("{}".format(my_list[count]), end="")
-        except:
+        except (IndexError, TypeError):
             break
         count += 1
-    if (count):
-        print("")
+    print("")
     return (count)
