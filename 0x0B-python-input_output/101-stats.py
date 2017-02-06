e6869407a212bbc7c6dcb8907f75cfb0771f87f7
Anne <anne.cognet@holbertonschool.com>
2017-02-06 15:13:08 +0000

    should do re, but failed

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import signal
+import re
 """
 This is module 101-stats
 This module parses a log
@@ -18,16 +19,16 @@ def parse_stdin():
     size = 0
     count = 0
     status_codes = {}
+    possibles = ["200", "301", "400", "401", "403", "404", "405", "500"]
     try:
         for line in sys.stdin:
             line_split = line.split()
-            try:
+            if line_split[-1].isdecimal():
                 size += int(line_split[-1])
-            except TypeError:
-                continue
+                if line_split[-2] in possibles:
+                    status_codes[line_split[-2]] = status_codes.get(
+                        line_split[-2], 0) + 1
             count += 1
-            status_codes[line_split[-2]] = status_codes.get(line_split[-2],
-                                                            0) + 1
             if count % 10 == 0:
                 print("File size: {:d}".format(size))
                 print_sorted_dict(status_codes)
