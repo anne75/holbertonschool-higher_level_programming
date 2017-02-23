1238470e19df05e055e10636e8a7d96128c04231
Anne <anne.cognet@holbertonschool.com>
2017-02-23 03:48:55 +0000

    Ian's

--- 0x0C-SQL_introduction/101-avg_temperatures.sql
+++ 0x0C-SQL_introduction/101-avg_temperatures.sql
@@ -1,7 +1,13 @@
 -- Displays the average temperature by city
 -- Sort by temperature descending
+-- SELECT
+-- city, AVG(`value`) AS avg_tmp
+-- FROM temperatures
+-- GROUP BY city
+-- ORDER BY avg_tmp DESC;
+-- Displays the average temperature by city (DESC)
 SELECT
-`city`, AVG(`value`) AS `avg_tmp`
-FROM `temperatures`
-GROUP BY `city`
-ORDER BY `avg_tmp` DESC;
+    city, AVG(value) AS avg_temp
+    FROM temperatures
+    GROUP BY city
+    ORDER BY avg_temp DESC;
