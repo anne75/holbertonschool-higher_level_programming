1ab607a03107103b94e60dadbf5fa6c9a59741e0
Anne <anne.cognet@holbertonschool.com>
2017-03-01 03:49:04 +0000

    capitalize

--- 0x0D-SQL_more_queries/102-rating_shows.sql
+++ 0x0D-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Use the database hbtn_0d_tvshows_rate
 -- List all shows and their ratings by popularity decreasing
 
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) as 'rating'
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
 GROUP BY tv_shows.title
