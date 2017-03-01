021267a57d01544e86031455747d0e3f30ac72dc
Anne <anne.cognet@holbertonschool.com>
2017-03-01 04:34:56 +0000

    103, one select

--- 0x0D-SQL_more_queries/103-rating_genres.sql
+++ 0x0D-SQL_more_queries/103-rating_genres.sql
@@ -2,13 +2,10 @@
 -- List all genres by decreasing order of popularity
 
 
-SELECT tv_genres.name, b.rating
+SELECT tv_genres.`name`, SUM(tv_show_ratings.rate) AS 'rating'
 FROM tv_genres
-INNER JOIN (
-      SELECT tv_show_genres.genre_id AS a, SUM(tv_show_ratings.rate) AS 'rating'
-      FROM tv_show_genres
-      INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
-      GROUP BY tv_show_genres.genre_id) AS b
-ON tv_genres.id = b.a
-ORDER BY b.rating DESC
+LEFT OUTER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+LEFT OUTER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_show_genres.show_id
+GROUP BY tv_genres.`name`
+ORDER BY rating DESC
 ;
