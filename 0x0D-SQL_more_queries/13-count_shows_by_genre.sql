8de785c7ba748cbb0725fe63b8d9804ad690e09d
Anne <anne.cognet@holbertonschool.com>
2017-03-01 00:20:02 +0000

    change col name

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,9 +2,9 @@
 -- List all genres and the number of times they are referenced
 -- Use only one S clause
 
-SELECT tv_genres.`name`, COUNT(tv_show_genres.genre_id) as 'number_shows'
+SELECT tv_genres.`name`, COUNT(tv_show_genres.genre_id) as 'number of shows'
 FROM tv_show_genres
 INNER JOIN tv_genres on tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.`name`
-ORDER BY `number_shows` DESC
+ORDER BY 2 DESC
 ;
