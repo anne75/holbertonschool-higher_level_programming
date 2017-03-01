36e5b4ba44ca3598282211e4a721bad2a03b5f12
Anne <anne.cognet@holbertonschool.com>
2017-03-01 00:00:49 +0000

    add 2 questions

---
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- In db passed by argument hbtn_od_tvshows
+-- List all genres and the number of times they are referenced
+-- Use only one SELECT clause
+
+SELECT tv_genres.`name`, COUNT(*) as 'number_shows'
+FROM tv_genres
+INNER JOIN tv_show_genres on tv_genres.id = tv_show_genres.genre_id
+GROUP BY `name`
+ORDER BY number_shows DESC
+;
