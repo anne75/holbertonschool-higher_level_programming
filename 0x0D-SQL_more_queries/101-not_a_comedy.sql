db1945cc9e121f0d0e27d3a630e5186f8628ad88
Anne <anne.cognet@holbertonschool.com>
2017-03-01 03:31:16 +0000

    add order by

--- 0x0D-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0D-SQL_more_queries/101-not_a_comedy.sql
@@ -9,5 +9,5 @@ WHERE tv_shows.title not in (
       INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
       INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
       WHERE tv_genres.`name` = 'Comedy')
--- ORDER BY tv_shows.title ASC
+ORDER BY tv_shows.title ASC
 ;
