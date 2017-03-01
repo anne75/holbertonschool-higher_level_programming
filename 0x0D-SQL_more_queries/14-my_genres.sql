cc00753435ba71698cfc07d74bf50de1e81b57ac
Anne <anne.cognet@holbertonschool.com>
2017-03-01 00:51:45 +0000

    add distinct

--- 0x0D-SQL_more_queries/14-my_genres.sql
+++ 0x0D-SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 -- List all genres for the show Dexter
 -- Only on S clause
 
-SELECT tv_genres.`name`
+SELECT DISTINCT tv_genres.`name`
 FROM tv_show_genres
 INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
