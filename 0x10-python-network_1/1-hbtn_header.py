aa9e87d54a1f16571da5c8ef013ab03d58b3808a
Anne <anne.cognet@holbertonschool.com>
2017-04-11 20:20:43 +0000

    use with

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -9,6 +9,5 @@ import sys
 
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1], method="HEAD")
-    response = urllib.request.urlopen(req)
-    print(response.getheader("X-Request-Id"))
-    # response.close()
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader("X-Request-Id"))
