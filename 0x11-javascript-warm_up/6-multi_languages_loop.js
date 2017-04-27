8572fee9928d20b6584dbda62a324abfd0d6f07f
Anne <anne.cognet@holbertonschool.com>
2017-04-27 21:45:01 +0000

    make a loop

--- 0x11-javascript-warm_up/6-multi_languages_loop.js
+++ 0x11-javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 const dubiousStatements = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-dubiousStatements.forEach((element) => { console.log(element); });
+// dubiousStatements.forEach((element) => { console.log(element); });
+for (const a of dubiousStatements) {
+  console.log(a);
+}
