ed1dd16875f2df571aeb21c817430a9b869f65cc
Anne <anne.cognet@holbertonschool.com>
2017-04-30 14:55:08 +0000

    add 5, add coment, do not instantiate s if < 0

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
+// create a child prototype Square inheriting from Rectangle
+// Square takes in one parameter, size
+
 const Rectangle = require('./4-rectangle').Rectangle;
 
-exports.Square = function (size) {
+Square = function (size) {
   Rectangle.call(this, size, size);
-  this.size = size;
+  if (size > 0) {
+    this.size = size;
+  }
 };
 
-exports.Square.prototype = Object.create(Rectangle.prototype);
-exports.Square.constructor = exports.Square;
+Square.prototype = Object.create(Rectangle.prototype);
+Square.constructor = Square;
+
+exports.Square = Square;
