bc7af0f0002cc1728bf3c573bbda257f7daf5f4d
Anne <anne.cognet@holbertonschool.com>
2017-05-01 02:56:32 +0000

    remove Rectangle proto thing

--- 0x12-javascript-web_scraping/5-square.js
+++ 0x12-javascript-web_scraping/5-square.js
@@ -8,7 +8,7 @@ function Square (size) {
   Rectangle.call(this, size, size);
 }
 
-Square.prototype = Object.create(Rectangle.prototype);
-Square.constructor = Square;
+// Square.prototype = Object.create(Rectangle.prototype);
+// Square.constructor = Square;
 
 exports.Square = Square;
