af857da55561b245026b496d299f62e63416d246
Anne <anne.cognet@holbertonschool.com>
2017-05-01 03:28:24 +0000

    add 4

--- 0x12-javascript-web_scraping/4-rectangle.js
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
+// create a class rectangle with 2 attributes and 3 methods
+
 function Rectangle (w, h) {
   if ((w > 0) && (h > 0)) {
     this.width = w;
     this.height = h;
   }
   this.print = function () {
-    if (w > 0 && h > 0) {
-      for (let i = 0; i < this.height; i += 1) {
-        console.log('X'.repeat(this.width));
-      }
+//    if (w > 0 && h > 0) {
+    for (let i = 0; i < this.height; i += 1) {
+      console.log('X'.repeat(this.width));
     }
+//    }
   };
   this.rotate = function () {
     const tmp = this.width;
