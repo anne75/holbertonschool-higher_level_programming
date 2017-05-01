2dd96acf2402838b9bbefc7520fd9b386871019d
Anne <anne.cognet@holbertonschool.com>
2017-05-01 05:43:29 +0000

    remove new line

--- 0x12-javascript-web_scraping/8-writeme.js
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
+//read a string (arg 3) and write to a file (arg 2)
+
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3] + '\n', 'utf8', function (err) {
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
   if (err) {
     return console.log(err);
   }
