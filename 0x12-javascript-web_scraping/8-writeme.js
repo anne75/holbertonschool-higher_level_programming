c95d1e2d3aadbcc87c0d9709f30fb91299c2ffe6
Anne <anne.cognet@holbertonschool.com>
2017-05-01 05:45:06 +0000

    ss

--- 0x12-javascript-web_scraping/8-writeme.js
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-//read a string (arg 3) and write to a file (arg 2)
+// read a string (arg 3) and write to a file (arg 2)
 
 const fs = require('fs');
 fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
