59bb033c131d740ea0ad23e0450d36cc23e6739a
Anne <anne.cognet@holbertonschool.com>
2017-05-03 02:32:20 +0000

    add 101

---
+++ 0x13-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $('div#add_item').on('click', function (event) {
+    $('UL.my_list').append('<LI>Item</LI>');
+  });
+  $('div#remove_item').on('click', function (event) {
+    $('UL.my_list LI:last').remove();
+  });
+  $('div#clear_list').on('click', function (event) {
+    $('UL.my_list LI').remove();
+  })
+});
