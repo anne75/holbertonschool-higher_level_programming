24ed380ab0f9894bfe7534840fee4d6b2f46b0a1
Anne <anne.cognet@holbertonschool.com>
2017-05-03 03:44:41 +0000

    add 102

---
+++ 0x13-javascript-web_jquery/102-script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  const url = 'https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json';
+  $('INPUT#btn_search').on('click', function () {
+    let cityName = $('INPUT#city_search').val();
+    // cityName = cityName.replace(' ', '%20');
+    const newUrl = url.replace('city_name', cityName);
+    $.getJSON(newUrl,
+      function (res) {
+        $('DIV#wind_speed').text(res.query.results.channel.wind.speed);
+      }
+     );
+  });
+});
